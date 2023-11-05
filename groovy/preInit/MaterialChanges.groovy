66f818e493013768b2457d2e184f3cac14769911
planetme <87911459+planetme@users.noreply.github.com>
2023-11-05 03:14:16 -0600

    init commit (req ceu 2.8)

--- groovy/preInit/MaterialChanges.groovy
+++ groovy/preInit/MaterialChanges.groovy
@@ -1,13 +1,27 @@
 import net.minecraftforge.fml.common.eventhandler.EventPriority
-import gregtech.api.unification.material.event.MaterialEvent;
+import gregtech.api.unification.material.event.MaterialEvent
+import gregtech.api.fluids.*
 
 import material.SuSyMaterials
 import classes.ChangeFlags
 
 log.infoMC("Registering new material event listener")
 
+private static void renameFluid(Material material, FluidStorageKey fluidStorageKey, String translationKey) {
+    FluidProperty property = material.getProperty(PropertyKey.FLUID);
+    if (property == null) throw new IllegalArgumentException("Material requires fluid property");
+
+    FluidStorage storage = property.getStorage();
+    FluidBuilder builder = storage.getQueuedBuilder(fluidStorageKey);
+    if (builder == null) throw new IllegalArgumentException("No fluid associated with storage key");
+    builder.translation(translationKey);
+}
+
 eventManager.listen(EventPriority.LOWEST) {
     MaterialEvent event ->
+        log.infoMC("Renaming fluids")
+
+        renameFluid(Materials.Polybenzimidazole, FluidStorageKeys.LIQUID, "removed_polybenzimidazole")
 
         log.infoMC("Starting new material registration")
         
